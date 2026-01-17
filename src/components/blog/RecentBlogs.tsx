c0c4082a43ef506c479e831a57fc5cc516cc9c8e
Abubakar dahir hassan <Abu112abu112abu112@gmail.com>
2026-01-17 12:08:50 +0530

    Adding a hover styles to blog, and main page's header

--- src/components/blog/RecentBlogs.tsx
+++ src/components/blog/RecentBlogs.tsx
@@ -21,7 +21,7 @@ export default function RecentBlogs() {
                 <div className="flex flex-wrap items-center justify-center gap-8 relative">
                     {
                         recentBlogs.map(blog => (
-                            <div key={blog.img} className="w-80 flex justify-center items-center flex-col p-4 shadow-[0_0_0_1px_rgba(0,0,0,0.05),0_12px_30px_rgba(0,0,0,0.18)] rounded-2xl">
+                            <div key={blog.img} className="w-80 flex justify-center items-center flex-col p-4 shadow-[0_0_0_1px_rgba(0,0,0,0.05),0_12px_30px_rgba(0,0,0,0.18)] rounded-2xl cursor-pointer hover:scale-105 duration-300">
                                 <img src={blog.img} alt="blog_image" width={270} height={350}/>
                                 <p className="font-semibold my-3">{blog.heading}</p>
                                 <p className="text-sofolBlack/50 text-sm">{blog.preview}</p>
