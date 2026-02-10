220333f3c50cd3a1139e00e37ba85611ce0f68bc
Abubakar dahir hassan <Abu112abu112abu112@gmail.com>
2026-02-10 23:54:37 +0530

    Removing unused imports to remove deployment errors in netlify

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import About from './Sections/About'
 import Footer from './Sections/Footer'
-import Hero from './Sections/Hero'
 import Home from './Sections/Home'
 import News from './Sections/News'
 import Pricing from './Sections/Pricing'
