6a6636098ebb354a1522f59a298cb7857ef37c1a
Abubakar dahir hassan <Abu112abu112abu112@gmail.com>
2026-02-10 23:55:32 +0530

    Making the Inputs in the contact page animated

---
+++ src/components/Input.tsx
@@ -0,0 +1,26 @@
+interface InputInterface {
+    type: string,
+    placeholder: string,
+    className?: string
+}
+
+export default function Input({type, placeholder, className} : InputInterface) {
+  return (
+    <div className={`relative ${className}`}>
+        <input type={type} id={type} placeholder=" " className="relative w-full px-2 py-3 border-2 border-black outline-none peer rounded-sm" />
+        <label htmlFor={type} 
+            className="
+                absolute top-4 left-3 text-sm duration-400 px-2 rounded-sm
+                peer-focus:-top-2.5
+                peer-focus:bg-white
+                peer-placeholder-shown:top-4
+                peer-placeholder-shown:left-3
+                peer-not-placeholder-shown:-top-2.5
+                peer-not-placeholder-shown:bg-white
+                text-gray-800
+                
+            "
+        >{placeholder}</label>
+    </div>
+  )
+}
--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,3 +1,5 @@
+import Input from "../Input";
+
 export default function Contact() {
   return (
     <section>
@@ -15,16 +17,16 @@ export default function Contact() {
                 <p className="uppercase font-semibold text-gray-700 my-2 max-sm:text-center">leave us a message</p>
                 <div className="flex gap-4 justify-between w-full max-sm:w-full max-sm:flex-col max-sm:items-center max-sm:justify-center">
                     <form action="" className="flex flex-col mr-4 w-9/20 max-sm:w-full">
-                        <input type="text" name="" id="" placeholder="first name and last name"  className="px-2 py-2.5 border-2 border-gray-600 rounded-sm my-1 text-xs"/>
-                        <input type="email" name="" id="" placeholder="email" className="px-2 py-2.5 border-2 border-gray-600 rounded-sm my-1 text-xs" />
-                        <textarea name="" id="" cols={4} rows={10} placeholder="your message" className="resize-none px-2 py-2.5 border-2 border-gray-600 rounded-sm my-1 text-xs"></textarea>
+                        <Input type="text" placeholder="First name and last name" className="mb-2.5"/>
+                        <Input type="email" placeholder="Email" className="mb-2.5"/>
+                        <textarea name="" id="" cols={4} rows={10} placeholder="your message" className="resize-none px-2 py-2.5 border-2 border-black rounded-sm my-1 text-sm text-gray-800 outline-none"></textarea>
                         <button type="submit" className="bg-sofolRed text-white capitalize cursor-pointer hover:bg-sofolRed/70 w-full rounded-xl py-3 mt-4">submit</button>
                     </form>
 
                     <div className="w-9/20 max-sm:w-full">
-                        <p className="flex gap-2 my-1 text-gray-700 cursor-pointer"><img src="/public/contact/phone.svg" alt="phone" width={15} height={15} /> +91-311-1234567</p>
-                        <p className="flex gap-2 my-1 text-gray-700 cursor-pointer"><img src="/public/contact/email.svg" alt="email" width={15} height={15} /> myportfolio@gmail.com</p>
-                        <p className="flex gap-2 my-1 text-gray-700 cursor-pointer"><img src="/public/contact/location.svg" alt="location" width={15} height={15} /> Address, Haryana, India</p>
+                        <p className="flex gap-2 my-2 text-gray-700 cursor-pointer text-sm"><img src="/public/contact/phone.svg" alt="phone" width={15} height={15} /> +91-311-1234567</p>
+                        <p className="flex gap-2 my-2 text-gray-700 cursor-pointer text-sm"><img src="/public/contact/email.svg" alt="email" width={15} height={15} /> myportfolio@gmail.com</p>
+                        <p className="flex gap-2 my-2 text-gray-700 cursor-pointer text-sm"><img src="/public/contact/location.svg" alt="location" width={15} height={15} /> Address, Haryana, India</p>
 
                         <img src="/contact/map.png" alt="map" width={270} height={180} className="mt-4" />
                     </div>
